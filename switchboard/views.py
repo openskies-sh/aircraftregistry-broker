3d3fad7bedacfcdc5bb3884320b4d51cf23916f6
Hrishikesh Ballal <hballal@gmail.com>
2020-09-13 21:30:01 +0100

    Update views.py

--- switchboard/views.py
+++ switchboard/views.py
@@ -26,7 +26,7 @@ from django.contrib.auth.views import redirect_to_login
 from django.core.exceptions import ImproperlyConfigured
 from django.contrib.auth import REDIRECT_FIELD_NAME
 from django.conf import settings
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class AccessMixin:
     """
@@ -68,12 +68,12 @@ class AccessMixin:
         return redirect_to_login(self.request.get_full_path(), self.get_login_url(), self.get_redirect_field_name())
 
 
-class LoginRequiredMixin(AccessMixin):
-    """Verify that the current user is authenticated."""
-    def dispatch(self, request, *args, **kwargs):
-        if not request.user.is_authenticated:
-            return self.handle_no_permission()
-        return super().dispatch(request, *args, **kwargs)
+# class LoginRequiredMixin(AccessMixin):
+#     """Verify that the current user is authenticated."""
+#     def dispatch(self, request, *args, **kwargs):
+#         if not request.user.is_authenticated:
+#             return self.handle_no_permission()
+#         return super().dispatch(request, *args, **kwargs)
 
 # class AjaxableResponseMixin:
 
